49c949c0f0ca9ea96834e0ac958023bd414d718b
Monishg2002 <Monishg2002@gmail.com>
2026-05-20 15:19:39 +0530

    Connect Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.SFC_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAwRET4NLr9MOE88_EB2eOVI8fYIaoDcXc",
+  authDomain: "star-feast-cafe.firebaseapp.com",
+  projectId: "star-feast-cafe",
+  storageBucket: "star-feast-cafe.firebasestorage.app",
+  messagingSenderId: "903959184840",
+  appId: "1:903959184840:web:9af2618d7e737e5744ecfd",
+  measurementId: "G-0ZNE34KVYJ"
 };
